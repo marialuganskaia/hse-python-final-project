bc0c3219a5c6d54f36eaf8a2f61034995a8ce1c1
Злата <zvzolotovitskaya@edu.hse.ru>
2025-12-13 21:50:13 +0300

    starter pack of bot

---
+++ final_project/src/hackathon_assistant/adapters/bot/routers.py
@@ -0,0 +1,8 @@
+from aiogram import Dispatcher
+
+from .user import user_router
+from .admin import admin_router
+
+def setup_routers(dp: Dispatcher) -> None:
+    dp.include_router(user_router)
+    dp.include_router(admin_router)
---
+++ final_project/src/hackathon_assistant/adapters/bot/user.py
@@ -0,0 +1,5 @@
+from aiogram import Router
+
+user_router = Router(name="user_router")
+
+# TODO: сюда позже добавятся хендлеры /start, /help, /hackathon, /schedule, /rules, /faq, /notify_on, /notify_off
